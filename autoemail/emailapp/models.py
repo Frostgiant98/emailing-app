82e4b629e9bca1fcaf5b5744d40be5a807c110ac
Frostgiant98 <niceice98@gmail.com>
2022-07-20 20:55:03 -0400

    added the forms and saving input to db

--- autoemail/emailapp/models.py
+++ autoemail/emailapp/models.py
@@ -1,3 +1,11 @@
+import email
+from operator import mod
 from django.db import models
 
 # Create your models here.
+
+class Mail(models.Model):
+    mail_id = models.AutoField(primary_key=True)
+    emailad = models.EmailField(null=False)
+    mailsub = models.CharField(max_length=70)
+    mailtext = models.CharField(default='' ,max_length=256)
--- autoemail/emailapp/views.py
+++ autoemail/emailapp/views.py
@@ -1,3 +1,32 @@
+from audioop import reverse
+from multiprocessing import context
+from django.urls import reverse
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .models import Mail
+
 
 # Create your views here.
+
+
+def homepage(request):
+    if request.method == "POST":
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        mail = request.POST.get('mail')
+        post = Mail( emailad = email , mailsub = subject, mailtext = mail)
+        post.save
+        reverse('homepage')
+
+    eve = Mail.objects.all()
+    paginator = Paginator(eve, 10)
+    page = request.GET.get('page')
+    try:
+        studs = paginator.page(page)
+    except PageNotAnInteger:
+        studs = paginator.page(1)
+    except EmptyPage:
+        studs = paginator.page(paginator.num_pages)
+
+    context = {'studs':studs}
+    return render(request,'index.html', context)
